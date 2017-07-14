8e63980d94d6eaea7f16e0872ee22f45b4994e6e
Gleb Kalachev <megas781@gmail.com>
2017-07-15 00:59:29 +0300

    Эх, забыл про поле с начальным кол-вом civilpoint'ов

--- Civilia/AddCMViewController.swift
+++ Civilia/AddCMViewController.swift
@@ -10,17 +10,53 @@ import UIKit
 
 class AddCMViewController: UIViewController {
    
+   //MARK: Outlets
+   
+   @IBOutlet weak var scrollView: UIScrollView!
    @IBOutlet weak var contentView: UIView!
+   @IBOutlet weak var nameTextField: UITextField!
+   @IBOutlet weak var segmentedControl: UISegmentedControl!
+   @IBOutlet weak var imageButton: UIButton!
+   
    
    
    override func viewDidLoad() {
       super.viewDidLoad()
       
+      do {
+         let gr = UITapGestureRecognizer.init(target: self, action: #selector(self.didTapContentView(sender:)))
+         self.contentView.addGestureRecognizer(gr)
+      }
       
    }
    
-   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-      print("touches: \(touches)")
+   //Чтобы убирать клавиатуру
+   func didTapContentView(sender: UITapGestureRecognizer) {
+      print("didtapContentView executed")
+      self.nameTextField.resignFirstResponder()
    }
+   
+   
+   @IBAction func segmentedControlTapped(_ sender: UISegmentedControl) {
+      self.nameTextField.resignFirstResponder()
+      
+      //Здесь мы будем смотреть на выбранный сегмент и, в зависимости от него, будем презентовать тот или иной view
+      
+      
+   }
+   
+   
+   @IBAction func imageButtonTapped(sender: UIButton) {
+      self.nameTextField.resignFirstResponder()
+      
+      //Здесь мы должны показать actionSheet с выбором imageSource
+      
+   }
+   
+   
+   @IBAction func returnKeyTapped(_ sender: UITextField) {
+      self.nameTextField.resignFirstResponder()
+   }
+   
    
 }
