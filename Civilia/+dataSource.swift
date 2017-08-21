d475ba4e0b8985be093de7f5e2634e3992c6f623
Gleb Kalachev <megas781@gmail.com>
2017-08-21 10:59:56 +0300

    Alpha #108
    Now data is loaded from RAM

--- Civilia/+dataSource.swift
+++ Civilia/+dataSource.swift
@@ -12,14 +12,14 @@ extension MainViewController {
    
    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
       //Когда выйдет Swift 4
-      return civilmakers.count
+      return inMemoryCivilmakers.count
    }
    
    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
       
       let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! GeneralTableViewCell
       
-      cell.setup(with: self.civilmakers[indexPath.row], viewController: self, indexPath: indexPath)
+      cell.setup(with: self.inMemoryCivilmakers[indexPath.row], viewController: self, indexPath: indexPath)
       
       
       
@@ -41,8 +41,13 @@ extension MainViewController {
       
       switch editingStyle {
       case .delete:
+         print("нахуй ты еще раз что-то удаляешь?")
+         print("Ну вот тебе еще один indexPath: \(indexPath)")
+         
+         fatalError("пока не лезь сюда")
+         
          try! realm.write {
-            realm.delete(self.civilmakers[indexPath.row])
+            realm.delete(self.inMemoryCivilmakers[indexPath.row])
          }
          tableView.deleteRows(at: [indexPath], with: .left)
       case .insert:
@@ -55,12 +60,16 @@ extension MainViewController {
    
    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
       let delete = UITableViewRowAction.init(style: .destructive, title: "Delete") { (action, indexPath) in
+         print("indexPath: \(indexPath)")
          
          try! realm.write {
-            realm.delete(self.civilmakers[indexPath.row])
+            realm.delete(self.inMemoryCivilmakers[indexPath.row])
          }
          
-         tableView.deleteRows(at: [indexPath], with: .automatic)
+         //После удаления обновляем наш основной in-memory массив
+         self.inMemoryCivilmakers = self.computedCivilmakers
+         
+         tableView.deleteRows(at: [indexPath], with: .fade)
          
       }
       return [delete]
